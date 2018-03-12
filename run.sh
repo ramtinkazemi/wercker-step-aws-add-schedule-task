e9456b1f086d0daa969894a74aa43aa33300a77a
Ramtin Kazemi <ramtin.kazemi@cashrewards.com>
2018-03-12 15:08:40 +1100

    initial commit

--- run.sh
+++ run.sh
@@ -57,6 +57,10 @@ if [ -z "$(step_var 'REGION')" ]; then
   exit 1
 fi
 
+if [ -z "$(step_var 'APP_NAME')" ]; then
+  error "Please set the 'app-name' variable"
+  exit 1
+fi
 if [ -z "$(step_var 'SCHEDULE_RULE_NAME')" ]; then
   error "Please set the 'schedule-rule-name' variable"
   exit 1
--- src/main.sh
+++ src/main.sh
@@ -63,7 +63,7 @@ cat $TARGET_FILE
 warn "Put rule : $STEP_SCHEDULE_RULE_NAME"
 aws events put-rule \
     --profile ${STEP_AWS_PROFILE} \
-    --name ${STEP_SCHEDULE_RULE_NAME} \
+    --name "$APP_NAME-$STEP_SCHEDULE_RULE_NAME" \
     --schedule-expression "${STEP_SCHEDULE_EXPRESSION}" \
     --state ${STEP_SCHEDULE_STATE} \
     --description  "${STEP_SCHEDULE_DESCRIPTION}"
